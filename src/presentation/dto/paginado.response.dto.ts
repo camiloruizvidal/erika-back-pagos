3b350b8722cd95177181fc63e4530dc302643ba8
camilo ruiz <camiloruizvidal@gmail.com>
2025-11-13 16:35:08 -0500

    Creando Paginado

---
+++ src/presentation/dto/paginado.response.dto.ts
@@ -0,0 +1,28 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { Expose, Type } from 'class-transformer';
+
+export class MetaPaginadoResponseDto {
+  @ApiProperty({ description: 'Total de registros disponibles', type: Number })
+  @Expose({ name: 'total' })
+  total!: number;
+
+  @ApiProperty({ description: 'Número de página solicitada (base 1)', type: Number })
+  @Expose({ name: 'pagina' })
+  pagina!: number;
+
+  @ApiProperty({ description: 'Cantidad de elementos por página', type: Number })
+  @Expose({ name: 'tamanoPagina' })
+  tamano_pagina!: number;
+}
+
+export class PaginadoResponseDto<T> {
+  @ApiProperty({ type: () => MetaPaginadoResponseDto })
+  @Expose({ name: 'meta' })
+  @Type(() => MetaPaginadoResponseDto)
+  meta!: MetaPaginadoResponseDto;
+
+  @ApiProperty({ isArray: true, type: () => Object })
+  @Expose({ name: 'data' })
+  data!: T[];
+}
+
